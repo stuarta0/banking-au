43c6660c4c16a15b3f46576b3fda9f973227e086
Stuart Attenborrow <stuarta0@gmail.com>
2016-08-04 21:33:35 +1000

    Documentation added

--- lib/BankingAU/ABA/Validation/Builtins/FileTotalRecordValidator.cs
+++ lib/BankingAU/ABA/Validation/Builtins/FileTotalRecordValidator.cs
@@ -7,6 +7,7 @@ namespace Banking.AU.ABA.Validation.Builtins
 {
     /// <summary>
     /// Validates an individual FileTotalRecord object.
+    /// Note: only checks against currency bounds.  Use <see cref="Banking.AU.ABA.Validation.AbaFile.FileTotalValidator"/> to also validate correct totals.
     /// </summary>
     public class FileTotalRecordValidator : CompositeValidator<R.FileTotalRecord>
     {
